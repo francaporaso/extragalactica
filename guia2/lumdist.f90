b79398f52467232823b656a2d3e3f42a71ca64c2
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-09-08 16:33:51 -0300

    adding calculation for mag absolute

--- guia2/lumdist.f90
+++ guia2/lumdist.f90
@@ -8,9 +8,10 @@ program main
     
     implicit none
     
-    integer :: i
+    integer :: i, nskip=0
     integer :: usdss = 11, nrows=20000
-    real :: luminosity_distance
+
+    real :: corr_ab_u=-0.036, corr_ab_g=0.012, corr_ab_r=0.010, corr_ab_i=0.028, corr_ab_z=0.040
 
     character(len=20) :: spec_id
     character(len=20) :: obj_id
@@ -39,32 +40,70 @@ program main
         read(usdss,*)spec_id,obj_id,ra,dec,redshift,pet_u,pet_g,pet_r,pet_i,pet_z,mod_u,mod_g,mod_r,mod_i,mod_z,ext_u,ext_g,ext_&
         &r,ext_i,ext_z,petR50_r,petR90_r,fracDev_r,velDisp
         
-        if ((mod_r>=14.5).and.(mod_r<=17.7).and.(petR50_r>1.5)) then
-            ! Calculito...          
+        if (.not.((mod_r>=14.5).and.(mod_r<=17.7).and.(petR50_r>1.5))) then
+            nskip=nskip+1
+            cycle
         end if
+        
+        ! === Magnitudes absolutas
+        dl = luminosity_distance(redshift)
+        dist_mod = -(5*log10(dl) + 25)
 
+        M_abs_mod_u = mod_u - ext_u + dm + corr_ab_u
+        M_abs_mod_g = mod_g - ext_g + dm + corr_ab_g
+        M_abs_mod_r = mod_r - ext_r + dm + corr_ab_r
+        M_abs_mod_i = mod_i - ext_i + dm + corr_ab_i
+        M_abs_mod_z = mod_z - ext_z + dm + corr_ab_z
+
+        M_abs_pet_u = pet_u - ext_u + dm + corr_ab_u
+        M_abs_pet_g = pet_g - ext_g + dm + corr_ab_g
+        M_abs_pet_r = pet_r - ext_r + dm + corr_ab_r
+        M_abs_pet_i = pet_i - ext_i + dm + corr_ab_i
+        M_abs_pet_z = pet_z - ext_z + dm + corr_ab_z
+
+        ! === Colores
+        u_r = mod_u - mod_r
+        g_r = mod_g - mod_r
+
+        ! === Radios petro en kpc
+        da = 
+    
     end do
-    close(unit=usdss)
 
+    close(unit=usdss)
+    
+    
 end program main
     
-function luminosity_distance(z_obj)
+real function luminosity_distance(z_obj)
     use cosmotab
     implicit none
     real, intent(in) :: z_obj
-    integer :: j
-    real :: luminosity_distance
+    integer :: j 
 
     call locate(z, n, z_obj, j)
     luminosity_distance = (1.0+z_obj)*(chi(j) + (z_obj - z(j))*(chi(j+1)-chi(j))/(z(j+1)-z(j)))
 
 end function
 
-function absolute_magnitude(m_ap)
-    real, intent(in) :: m_ap
+real function angular_diam_distance(z_obj)
+    use cosmotab
+    implicit none
+    real, intent(in) :: z_obj
+    integer :: j
+    call locate(z, n, z_obj, j)
+    angular_diam_distance = 
 
-end function
+subroutine absolute_mag(z_obj, m_ap, ext, M_abs)
+    implicit none
+    real, intent(in) :: z_obj, m_ap, ext
+    real, intent(out) :: M_abs
 
+    dl = luminosity_distance(z_obj)
+    dist_mod = -( 5*log10(dl) + 25 )
+    M_abs = m_ap - ext + dist_mod + corr_ab
+
+end subroutine
 
 include 'locate.f'
 
