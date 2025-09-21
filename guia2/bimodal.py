00889f53d39f814a959d4262f143d067f8650a72
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-09-21 12:02:35 -0300

    changed fit_colors for fit_bimodal, now it can fit any given dist

--- guia2/bimodal.py
+++ guia2/bimodal.py
@@ -1,27 +1,27 @@
-from scipy.optimize import curve_fit, minimize
+from scipy.optimize import curve_fit
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scienceplots
+# import scienceplots
 
-plt.style.use('science')
-_params = {
-    'figure.figsize':(5.6,3.5),
-    'figure.dpi':100,
-    'font.family':'sans-serif',
-    'font.size':12,
-    'savefig.format':'png',
-    'savefig.transparent':True,
-    'xtick.direction':'in',
-    'xtick.top':True,
-    'ytick.direction':'in',
-    'ytick.right':True,
-    'errorbar.capsize':3,
-    'legend.loc':'upper right',
-    'legend.frameon':True,
-    'legend.fontsize':9,
-}
-plt.rcParams.update(_params)
+# plt.style.use('science')
+# _params = {
+#     'figure.figsize':(5.6,3.5),
+#     'figure.dpi':100,
+#     'font.family':'sans-serif',
+#     'font.size':12,
+#     'savefig.format':'png',
+#     'savefig.transparent':True,
+#     'xtick.direction':'in',
+#     'xtick.top':True,
+#     'ytick.direction':'in',
+#     'ytick.right':True,
+#     'errorbar.capsize':3,
+#     'legend.loc':'upper right',
+#     'legend.frameon':True,
+#     'legend.fontsize':9,
+# }
+# plt.rcParams.update(_params)
 
 _folder = 'figures/'
 
@@ -35,18 +35,10 @@ def bimodal(x, w1, mu1, sigma1, w2, mu2, sigma2):
 def chisq(yobs, yfit):
     return np.sum((yobs-yfit)**2/yfit)
 
-def fit_colors(color='u_r', nbins=50, plots=True):
+def fit_bimodal(dist, p0, nbins=50, plots=False):
 
-    G = pd.read_fwf('gals.dat')
-    G.query('u_r < 4.0 and g_r < 1.3 and c9050 > 1.5 and c9050 < 4', inplace=True)
-
-    y, xedges = np.histogram(G[color], bins=nbins, density=True)
+    y, xedges = np.histogram(dist, bins=nbins, density=True)
     x = 0.5*(xedges[:-1]+xedges[1:])
-
-    if color == 'u_r':
-        p0=[0.5, 1.5, 0.3, 0.5, 2.5, 0.2]
-    else:
-        p0=[0.6, 0.3, 0.1, 0.4, 0.8, 0.1]
     
     popt, cov = curve_fit(
         bimodal, 
@@ -64,12 +56,12 @@ def fit_colors(color='u_r', nbins=50, plots=True):
     if plots:
         plt.figure()
         
-        plt.stairs(edges=xedges, values=y, label='SDSS', hatch='///', color='dimgray')
+        plt.stairs(edges=xedges, values=y, hatch='///', color='dimgray')
         plt.plot(x, yfit, label='Ajuste bimodal', c='k', lw=2)
-        plt.plot(x, popt[0]*normal(x,popt[1],popt[2]), label='Nube azul', c='C0', ls='--')
-        plt.plot(x, popt[3]*normal(x,popt[4],popt[5]), label='Secuencia roja', c='C3', ls='--')
+        plt.plot(x, popt[0]*normal(x,popt[1],popt[2]), label='Comp 1', c='C0', ls='--')
+        plt.plot(x, popt[3]*normal(x,popt[4],popt[5]), label='Comp 2', c='C3', ls='--')
         
-        plt.xlabel('$u-r$' if color=='u-r' else '$g-r$')
+        plt.xlabel('Dist')
         plt.ylabel('Densidad de objetos')
 
         plt.legend(ncols=2)
@@ -79,11 +71,14 @@ def fit_colors(color='u_r', nbins=50, plots=True):
 
 if __name__ == '__main__':
 
-    g_r = fit_colors('g_r', plots=False)
-    u_r = fit_colors('u_r', plots=False)
+    G = pd.read_fwf('gals.dat')
+    G.query('u_r < 4.0 and g_r < 1.3 and c9050 > 1.5 and c9050 < 4', inplace=True)
+
+    g_r = fit_bimodal(G['g_r'], p0=[0.6, 0.3, 0.1, 0.4, 0.8, 0.1])
+    u_r = fit_bimodal(G['u_r'], p0=[0.5, 1.5, 0.3, 0.5, 2.5, 0.2])
 
     np.savetxt(
-        'colors_fit.dat', 
+        _folder+'colors_fit.dat', 
         np.vstack([
             u_r['popt'], 
             np.sqrt(np.diag(u_r['cov'])),
