2c6070b328f6a830b93ed5c657b27541576d6821
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-09-30 19:28:14 -0300

    finished plots

--- guia2/plots.py
+++ guia2/plots.py
@@ -7,7 +7,8 @@ import scienceplots
 
 plt.style.use('science')
 _params = {
-    'figure.figsize':(5,3),
+    'figure.figsize':(5,3.5),
+    #'figure.figsize':(6.47,4), # golden ratio
     'figure.dpi':96,
     'font.family':'sans-serif',
     'font.size':12,
@@ -21,44 +22,49 @@ _params = {
     'legend.loc':'upper right',
     'legend.frameon':True,
     'legend.fontsize':9,
+    #'axes.axisbelow':True,
 }
 plt.rcParams.update(_params)
 
 _folder = 'figures/'
 
+SDSS = pd.read_csv('SDSS_guia2_fmcaporaso.csv')
+
 G = pd.read_fwf('gals.dat')
 G.query('u_r < 4.0 and u_r > 0 and g_r < 1.3 and c9050 > 1.5 and c9050 < 4', inplace=True)
 
-def plot_problema2():
-
-    df = pd.read_csv('SDSS_guia2_fmcaporaso.csv')
+def plot_SDSSraw():
 
     # sky footprint
-    fig, ax = plt.subplots(1,1)
-    ax.scatter(df.ra, df.dec, s=3, alpha=0.8)
+    fig, ax = plt.subplots(figsize=(6.47,4),subplot_kw={'projection': 'mollweide'})
+    ax.grid(True, alpha=0.5)
+    ax.scatter(np.deg2rad(SDSS.ra)-np.pi, np.deg2rad(SDSS.dec), s=1, alpha=1, c='C1')
     ax.set_xlabel('Ascención recta [deg]')
     ax.set_ylabel('Declinación [deg]')
-    fig.savefig('sky_footprint.pdf')
+    ax.set_xticklabels(np.arange(30,360,30))
+    #fig.savefig('sky_footprint.pdf')
     #plt.show()
 
     # redshift distribution
-    fig, ax = plt.subplots(1,1)
-    ax.hist(df.redshift, bins=25, histtype='step')
+    fig, ax = plt.subplots()
+    ax.hist(SDSS.redshift, bins=30, histtype='step', color='dimgray', hatch='///')
     ax.set_xlabel('Redshift $z$')
     ax.set_ylabel('Número de galaxias')
-    fig.savefig('z_dist_25bins.pdf')
+    #fig.savefig('z_dist_25bins.pdf')
     #plt.show()
 
     # mag vs z
-    fig, ax = plt.subplots(1,1)
-    ax.scatter(df.petroMag_r, df.redshift, s=2, alpha=0.5, c='C0')
-    ax.scatter([],[],c='C0',s=8,label='Full Sample')
-    mask = (df.petroMag_r >= 14.5)&(df.petroMag_r <= 17.77)
-    ax.scatter(df.petroMag_r[mask], df.redshift[mask], s=4, alpha=0.5, c='C1')
-    ax.scatter([],[],c='C1',s=8,label='$14.5 \leq r \leq 17.77$')
-    ax.set_xlabel('Magnitud petrosiana banda $r$')
-    ax.set_ylabel('Redshift $z$')
-    ax.legend(loc='upper right', frameon=True)
+    fig, ax = plt.subplots()
+    ax.scatter(SDSS.redshift, SDSS.petroMag_r, s=1, alpha=0.5, c='C1')
+    #ax.scatter([],[],c='C0',s=8,label='SDSS')
+    ax.axhline(14.5, ls='--', c='k')
+    ax.text(0.051,18.4,'$\\boldsymbol{r = 17.77}$',ha='right')
+    ax.text(0.051,14.4,'$\\boldsymbol{r = 14.5}$',ha='right')
+    ax.axhline(17.77, ls='--', c='k')
+    ax.invert_yaxis()
+    ax.set_xlabel('Redshift $z$')
+    ax.set_ylabel('Magnitud aparente $r$')
+    #ax.legend(loc='upper right', frameon=True)
     #fig.savefig('r_vs_redshift.pdf')
     #plt.show()
 
@@ -90,7 +96,7 @@ def plot_bimodalcolors(savename=None):
              label='Sec. roja', c='r', ls='--')
     ax2.set_xlabel('$u-r$')
     ax2.legend(ncols=2)
-    fig.savefig(_folder+'colors_fit.png')
+    #fig.savefig(_folder+'colors_fit.png')
 
     if savename is not None:
         np.savetxt(
@@ -106,7 +112,7 @@ def plot_conc_fracdev():
     fig, ax = plt.subplots(1,3, figsize=(12,3.8))
     ax[0].hist(G['c9050'], bins=50, density=True, histtype='step', hatch='///', color='dimgray')
     ax[1].hist(G['fracDeV_r'], bins=50, density=True, histtype='step', hatch='///', color='dimgray')
-    ax[2].scatter(G['c9050'], G['fracDeV_r'], s=1, alpha=0.4)
+    ax[2].scatter(G['c9050'], G['fracDeV_r'], s=1, alpha=0.4, c='C1')
 
     ax[0].set_xlabel('Índice de concentración $C$')
     ax[0].set_ylabel('Densidad de galaxias')
@@ -118,7 +124,7 @@ def plot_conc_fracdev():
 
 def plot_conc_u_r():
     fig, ax = plt.subplots(figsize=(5,5))
-    ax.scatter(G['c9050'], G['u_r'], s=1, alpha=0.2, c='dimgray')
+    ax.scatter(G['c9050'], G['u_r'], s=1, alpha=0.2, c='C1')
     ax.axvline(2.5, ls='--', lw=1., c='k') # 
     ax.axhline(2.0, ls='--', lw=1., c='k') # color
     
@@ -248,9 +254,9 @@ def plot_luminositysize():
             axes[2].plot(samples[i]['M_pet_r'], samples[i]['M_pet_r']*a+b, c=colors[i], lw=2.0)
             axes[2].text(-22,0.95-0.08*(i-2),f'${a=:2.2f}, {b=:2.2f}$',fontsize=10, c=colors[i])
 
-    axes[1].scatter(samples[1]['M_pet_r'], np.log10(samples[1]['r50']), s=0.8, marker='s', c=s_red, alpha=0.3)
+    axes[1].scatter(samples[1]['M_pet_r'], np.log10(samples[1]['r50']), s=0.8, marker='o', c=s_red, alpha=0.3)
     axes[1].scatter(samples[0]['M_pet_r'], np.log10(samples[0]['r50']), s=0.8, marker='o', c=s_blue, alpha=0.3)
-    axes[2].scatter(samples[3]['M_pet_r'], np.log10(samples[3]['r50']), s=0.8, marker='s', c=s_early, alpha=0.3)
+    axes[2].scatter(samples[3]['M_pet_r'], np.log10(samples[3]['r50']), s=0.8, marker='o', c=s_early, alpha=0.3)
     axes[2].scatter(samples[2]['M_pet_r'], np.log10(samples[2]['r50']), s=0.8, marker='o', c=s_late, alpha=0.3)
 
     for i in range(3):
@@ -258,10 +264,10 @@ def plot_luminositysize():
     axes[0].set_ylabel(r'$\log_{10}(r_{50}/\mathrm{kpc})$')
 
     axes[1].plot([],[],'o', markersize=5, c=s_blue,label='$u-r<2.0$')
-    axes[1].plot([],[],'s', markersize=5, c=s_red,label='$u-r\geq2.0$')
+    axes[1].plot([],[],'o', markersize=5, c=s_red,label='$u-r\geq2.0$')
 
     axes[2].plot([],[],'o', markersize=5, c=s_late,label='$C<2.5$')
-    axes[2].plot([],[],'s', markersize=5, c=s_early,label='$C\geq2.5$')
+    axes[2].plot([],[],'o', markersize=5, c=s_early,label='$C\geq2.5$')
 
     axes[1].legend()
     axes[2].legend()
@@ -271,18 +277,18 @@ def plot_luminositysize():
     earlyred = samples[1][G['c9050']>=2.5]
     lateblue = samples[0][G['c9050']<2.5]
         
-    ax.scatter(earlyred['M_pet_r'], np.log10(earlyred['r50']), s=0.8, marker='o', c=s_red, alpha=0.5)
+    ax.scatter(earlyred['M_pet_r'], np.log10(earlyred['r50']), s=0.8, marker='o', c='r', alpha=0.5)
     (a,b), _ = curve_fit(linear, earlyred['M_pet_r'], np.log10(earlyred['r50']), p0=[1.0,-2.0])
     ax.plot(earlyred['M_pet_r'], earlyred['M_pet_r']*a+b, c=colors[1], lw=2.0)
     ax.text(-22,0.95,f'${a=:2.2f}, {b=:2.2f}$',fontsize=12, c=colors[1])
 
-    ax.scatter(lateblue['M_pet_r'], np.log10(lateblue['r50']), s=0.8, marker='o', c=s_blue, alpha=0.5)
+    ax.scatter(lateblue['M_pet_r'], np.log10(lateblue['r50']), s=0.8, marker='o', c='b', alpha=0.5)
     (a,b), _ = curve_fit(linear, lateblue['M_pet_r'], np.log10(lateblue['r50']), p0=[1.0,-2.0])
     ax.plot(lateblue['M_pet_r'], lateblue['M_pet_r']*a+b, c=colors[0], lw=2.0)
     ax.text(-22,0.87,f'${a=:2.2f}, {b=:2.2f}$',fontsize=12, c=colors[0])
     
-    ax.plot([],[],'o', markersize=5, c=s_red, label='Rojo + early')
-    ax.plot([],[],'o', markersize=5, c=s_blue, label='Azul + late')
+    ax.plot([],[],'o', markersize=5, c='r', label='Rojo + early')
+    ax.plot([],[],'o', markersize=5, c='b', label='Azul + late')
 
     ax.set_ylabel(r'$\log\left({r_{50}/\mathrm{kpc}}\right)$')
     fig2.text(0.5, 0.01, '$M_r$ petrosiana', ha='center')
@@ -292,21 +298,91 @@ def plot_luminositysize():
 
 def plot_kormendy():
     # x = logr50, y = mu50
-    mask = G['mu50']<24
-    earlyreds = mask&(G['u_r']>=2.0)&(G['c9050']>=2.5)
-    lateblues = mask&(G['u_r']<2.0)&(G['c9050']<2.5)
+    
+    linear = lambda x, a,b: a*x+b
+    
+    s_early = '#4daf4a'
+    s_late = '#984ea3'
+    s_blue = "#136cb4"
+    s_red = '#e41a1c'
 
-    m = [mask, earlyreds, lateblues]
-    c = ['dimgray', 'red', 'blue']
+    colors = [
+    "#0b375c",
+    "#690e10",
+    "#5c1d66",
+    "#1a6e17",]
+    
+    ### C grande -> luz concentrada en el centro -> early
+    ### C chico -> luz desparramada -> late
+    ### u-r grande -> mayor mag in r -> roja
+    ### u-r chico -> mayor mag in u -> azul
 
-    fig, axes = plt.subplots(1,3,figsize=(12,4),sharex=True,sharey=True)
+    samples = [G[(G['u_r']<2.0)&(G['mu50']<24.5)],
+               G[(G['u_r']>=2.0)&(G['mu50']<24.5)],
+               G[(G['c9050']<2.5)&(G['mu50']<24.5)],
+               G[(G['c9050']>=2.5)&(G['mu50']<24.5)]]
+
+    fig, axes = plt.subplots(1,3, figsize=(12,4), sharex=True, sharey=True)
+    fig.subplots_adjust(wspace=0.1, hspace=0.2)
+    for i in range(3): axes[i].invert_yaxis()
+
+    axes[0].scatter(np.log10(G['r50'][(G['mu50']<24.5)]), G['mu50'][(G['mu50']<24.5)], s=1, c='dimgray', alpha=0.3)
+    # for i in range(4):
+    #     (a,b), _ = curve_fit(linear, np.log10(samples[i]['r50']), samples[i]['mu50'], p0=[4.0,20.0])
+    #     if i<2:
+    #         axes[1].plot(np.log10(samples[i]['r50']), np.log10(samples[i]['r50'])*a+b, c=colors[i], lw=2.0)
+    #         # axes[1].text(-22,0.95-0.08*i,f'${a=:2.2f}, {b=:2.2f}$',fontsize=10, c=colors[i])
+    #     else:
+    #         axes[2].plot(np.log10(samples[i]['mu50']), np.log10(samples[i]['mu50'])*a+b, c=colors[i], lw=2.0)
+    #         # axes[2].text(-22,0.95-0.08*(i-2),f'${a=:2.2f}, {b=:2.2f}$',fontsize=10, c=colors[i])
+
+    axes[1].scatter(np.log10(samples[1]['r50']), samples[1]['mu50'], s=0.8, marker='o', c=s_red, alpha=0.3)
+    axes[1].scatter(np.log10(samples[0]['r50']), samples[0]['mu50'], s=0.8, marker='o', c=s_blue, alpha=0.3)
+    axes[2].scatter(np.log10(samples[3]['r50']), samples[3]['mu50'], s=0.8, marker='o', c=s_early, alpha=0.3)
+    axes[2].scatter(np.log10(samples[2]['r50']), samples[2]['mu50'], s=0.8, marker='o', c=s_late, alpha=0.3)
+
     for i in range(3):
-        axes[i].invert_yaxis()
-        axes[i].scatter(np.log10(G['r50'][m[i]]), G['mu50'][m[i]],s=1, c=c[i], alpha=0.3)
+        axes[i].set_xlabel(r'$\log_{10}(r_{50}/\mathrm{kpc})$')
+    axes[0].set_ylabel(r'$\mu_{50} \, \mathrm{[mag/arcsec^2]}$')
+
+    axes[1].plot([],[],'o', markersize=5, c=s_blue,label='$u-r<2.0$')
+    axes[1].plot([],[],'o', markersize=5, c=s_red,label='$u-r\geq2.0$')
+
+    axes[2].plot([],[],'o', markersize=5, c=s_late,label='$C<2.5$')
+    axes[2].plot([],[],'o', markersize=5, c=s_early,label='$C\geq2.5$')
+
+    axes[1].legend()
+    axes[2].legend()
+
+    fig2, ax = plt.subplots(figsize=(5,5))
+    ax.invert_yaxis()
+    
+    earlyred = samples[1][G['c9050']>=2.5]
+    lateblue = samples[0][G['c9050']<2.5]
+        
+    ax.scatter(np.log10(earlyred['r50']), earlyred['mu50'], s=0.8, marker='o', c='r', alpha=0.5)
+    # (a,b), _ = curve_fit(linear, np.log10(earlyred['r50']), earlyred['mu50'], p0=[1.0,-2.0])
+    # ax.plot(np.log10(earlyred['r50']), np.log10(earlyred['r50'])*a+b, c=colors[1], lw=2.0)
+    # ax.text(-22,0.95,f'${a=:2.2f}, {b=:2.2f}$',fontsize=12, c=colors[1])
+
+    ax.scatter(np.log10(lateblue['r50']), lateblue['mu50'], s=0.8, marker='o', c='b', alpha=0.5)
+    # (a,b), _ = curve_fit(linear, np.log10(lateblue['r50']), lateblue['mu50'], p0=[4.0,20.0])
+    # ax.plot(np.log10(lateblue['r50']), np.log10(lateblue['r50'])*a+b, c=colors[0], lw=2.0)
+    # ax.text(-22,0.87,f'${a=:2.2f}, {b=:2.2f}$',fontsize=12, c=colors[0]k)
+    
+    ax.plot([],[],'o', markersize=5, c='r', label='Rojo + early')
+    ax.plot([],[],'o', markersize=5, c='b', label='Azul + late')
+
+    ax.set_ylabel(r'$\mu_{50} \, \mathrm{[mag/arcsec^2]}$')
+    fig2.text(0.5, 0.01, r'$\log\left({r_{50}/\mathrm{kpc}}\right)$', ha='center')
+
+    ax.legend()
+
+
 
 if __name__=='__main__':
     
-    #plot_problema2()
+    plot_SDSSraw()
     plot_bimodalcolors()
     plot_conc_fracdev()
     plot_conc_u_r()
