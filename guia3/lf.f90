ddb22c7114f7067e66e73be7d7ad53d16f6c14b4
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-10-07 16:38:03 -0300

    update, saving LF phi(M)

--- guia3/lf.f90
+++ guia3/lf.f90
@@ -1,9 +1,9 @@
 program main
     implicit none
     
-    integer, parameter :: ugals = 11
+    integer, parameter :: ugals = 11, ulf = 12
     integer, parameter :: nrows = 472922
-    integer, parameter :: nbins=50
+    integer, parameter :: nbins=20
     integer :: i
     integer :: jbin
     real :: M_r, r, z_gal, wi
@@ -14,7 +14,6 @@ program main
     dx = (-16.0+23.0)/real(nbins)
     
     open(unit=ugals, file='gals_test.dat', status='old', action='read')
-    
     do i=1, nrows
         read(ugals, *) M_r, r, z_gal, wi
         jbin = int((M_r+23.0)/dx) + 1
@@ -22,8 +21,12 @@ program main
         bincount(jbin) = bincount(jbin) + 1
         phi(jbin) = phi(jbin) + wi
     end do
+    close(ugals)
     
-    print *, bincount
-    print *, phi
-    
+    open(unit=ulf, file='lumfunc_n20.dat', status='unknown')
+    do i=1,nbins
+        write(ulf, *) bincount(i), phi(i), -23.0+(i-1)*dx, -23.0+i*dx
+    end do
+    close(ulf)
+
 end program main
