f4bc6b39dfc46685daf6c2446d5a26aa348e8c43
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-10-16 17:53:29 -0300

    ignoring .13 files?

--- guia4/density.f90
+++ guia4/density.f90
@@ -1,10 +1,10 @@
 module galaxias
     implicit none
     integer, parameter :: ngals=5725
-    integer, dimension(ngals) :: cls_gal
-    integer, dimension(ngals) :: ig_gal ! galaxy index
-    integer, dimension(ngals) :: ty_gal ! tipo morf-> 1:E 2:S 3:Irr 4:Nosesabe
-    real, dimension(ngals) :: ra_gal, dec_gal
+    integer, allocatable :: cls_gal(:)
+    integer, allocatable :: ig_gal(:) ! galaxy index
+    integer, allocatable :: ty_gal(:) ! tipo morf-> 1:E 2:S 3:Irr 4:Nosesabe
+    real, allocatable :: ra_gal(:), dec_gal(:)
 end module galaxias
 
 module clusters
@@ -20,71 +20,77 @@ module const
     implicit none
     real, parameter :: pi = 4.0*atan(1.0)
     real, parameter :: d2r = pi/180.0
+    real, parameter :: H0 = 70.0 !km/s/Mpc
+    real, parameter :: cvel = 299792.458 ! km/s
 end module const
 
 program main
+    
     use const
     use galaxias
     use clusters
+    use numerical
+
     implicit none
-    integer, parameter :: ugals=11, ucluster=12
+    
+    integer, parameter :: ugals=11, ucluster=12, utable=13
     integer :: i, j, k
-    real, dimension(79) :: ra, dec
-    real :: sep
+    integer, allocatable :: indx_gal(:)
+    real :: sep, dist_cl
+    real, allocatable :: dist_gal(:)
+
+    real, external :: angular_distance
+    
+    open(unit=ugals, file='galaxias.dat', status='old')
+    read(ugals, *) ! read header
     
     open(unit=ucluster, file='centros.dat', status='old')
     read(ucluster, *) ! read header
-    read(ucluster, *) cls_cl, nmem, redshift_cl, ra_cl, dec_cl
-    close(ucluster)
-
-    open(unit=ugals, file='galaxias.dat', status='old')
-    read(ugals, *) ! read header
-    j = 1
-    do i=1, ngals
-        read(ugals, *) cls_gal(i), ig_gal(i), ra_gal(i), dec_gal(i), ty_gal(i)
-        if (cls_gal(i)==cls_cl) then
-            ra(j)=ra_gal(i)
-            dec(j)=dec_gal(i)
-            j = j+1
-        end if
-    end do
-    close(ugals)
     
-    do j=1, nmem
-        do k=1, nmem
-            if (k==j) cycle
-            call angular_distance(ra(j), dec(j), ra(k), dec(k), sep)
-            print *, sep
+    !open(unit=utable, file='distances.dat', status='unknown')
+
+    do i=1, ncluster
+        
+        read(ucluster, *) cls_cl, nmem, redshift_cl, ra_cl, dec_cl
+        dist_cl = redshift_cl*cvel/H0
+
+        allocate(cls_gal(nmem), ig_gal(nmem), ra_gal(i), dec_gal(nmem), ty_gal(nmem))
+        allocate(dist_gal(nmem), indx_gal(nmem))
+                
+        do j=1, nmem        
+            read(ugals, *) cls_gal(j), ig_gal(j), ra_gal(i), dec_gal(j), ty_gal(j)
         end do
-    end do
 
-    !probar para 1 cluster primero
-    ! do i=1,79
-    !     ra(i), dec(i)
-    ! end do
+        do j=1, nmem
+            do k=1, nmem
+                if (k==j) cycle
+                sep = angular_distance(ra_gal(j), dec_gal(j), ra_gal(k), dec_gal(k))
+                dist_gal(k) = tan(sep)*dist_cl
+            end do
 
-    ! do i=1, 79
-    !     a1 = ra(i)
-    !     d1 = dec(i)
-    !     do j=1,79
-    !         if (j==i) cycle
-    !         sep = angular_distance(a1, d1, ra(j), dec(j))
-    !     end do
-    ! end do
+            call indexx_sp(dist_gal, indx_gal)
+            print *, dist_gal(indx_gal(10))
+        
+        end do
+        
+        deallocate(cls_gal, ig_gal, ra_gal, dec_gal, ty_gal, dist_gal, indx_gal)
     
-
+    end do
+    
+    close(ucluster)
+    close(ugals)
+    !close(utable)
 
 end program main
 
-subroutine angular_distance(ra1, dec1, ra2, dec2, sep)
+real function angular_distance(ra1, dec1, ra2, dec2)
     use const
     implicit none
     real, intent(in) :: ra1, dec1, ra2, dec2
-    real, intent(out) :: sep
     real :: dra, cos_sep
 
     dra = abs(ra2-ra1)
     cos_sep = sin(d2r*dec1)*sin(d2r*dec2) + cos(d2r*dec1)*cos(d2r*dec2)*cos(d2r*dra)
-    sep = acos(cos_sep)/d2r
+    angular_distance = acos(cos_sep)
     
-end subroutine angular_distance
+end function angular_distance
