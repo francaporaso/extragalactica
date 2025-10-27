1c4a4fa8b6a353c3ee78b5e97c41bc4c267f23a5
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-10-27 12:58:18 -0300

    updater

--- guia3/lf.f90
+++ guia3/lf.f90
@@ -11,12 +11,14 @@ program main
     integer, dimension(nbins) :: bincount = 0
     real, dimension(nbins) :: phi = 0
     
-    dx = (-16.0+23.0)/real(nbins)
+    real, parameter :: Mr_min=-23.0, Mr_max=-16.0
+
+    dx = (Mr_max-Mr_min)/real(nbins)
     
     open(unit=ugals, file='gals_test.dat', status='old', action='read')
     do i=1, nrows
         read(ugals, *) M_r, r, z_gal, wi
-        jbin = int((M_r+23.0)/dx) + 1
+        jbin = int((M_r-Mr_min)/dx) + 1
         
         bincount(jbin) = bincount(jbin) + 1
         phi(jbin) = phi(jbin) + wi
